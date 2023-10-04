fbb5e62eb2bc7f93139963163b2d820b4ba2cc2e
PranshuPatle <146441402+PranshuPatle@users.noreply.github.com>
2023-10-04 13:29:31 +0000

    variables commit

---
+++ Basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = 123;
+
+let accountEmail = "pranshu@gmail.com";
+
+var accountPassword = 12345;
+
+accountCity = "Indore";
+
+let accountState; //answer - undefined
+
+//accountId = 234;
+
+accountEmail = "pp@gmail.com";
+
+var accountPassword = 555;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/* prefer to not use var because here is the issue of block scope */
