5a8adc681e82f19606e9437ab3704c9e71351490
PranshuPatle <146441402+PranshuPatle@users.noreply.github.com>
2023-10-18 11:15:21 +0000

    datatypes

---
+++ Basics/datatype.js
@@ -0,0 +1,8 @@
+"use strict" //treat the all js code as a new version of js 
+
+// alert(3+3); we can't use this, because we are using node js not browser
+
+// code should be readable, professional
+
+
+
